8ce53c2b167686d6b263ddad8644a1dc72a9dfe2
ratobing <andhiratobingg@gmail.com>
2022-06-12 16:54:22 +0700

    add TypeAlisUtils.kt

---
+++ core/abstraction/src/main/java/com/art/android/abstraction/TypeAlisUtils.kt
@@ -0,0 +1,12 @@
+package com.art.android.abstraction
+
+/**
+ * @author andhiratobing
+ * @project Recipes
+ */
+
+typealias UnitLambda = () -> Unit
+typealias BooleanLambda = (Boolean) -> Unit
+typealias StringLambda = (String) -> Unit
+typealias IntLambda = (Int) -> Unit
+typealias LongLambda = (Long) -> Unit
