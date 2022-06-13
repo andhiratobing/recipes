2c350e4cc8042006f548f2655acbe501d4960f4e
ratobing <andhiratobingg@gmail.com>
2022-06-13 19:49:54 +0700

    optimize imports

--- core/abstraction/src/test/java/com/art/android/abstraction/ExampleUnitTest.kt
+++ core/abstraction/src/test/java/com/art/android/abstraction/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.art.android.abstraction
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
--- features/recipes/build.gradle.kts
+++ features/recipes/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("common-compose-plugin")
+    id("dagger.hilt.android.plugin")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 dependencies {
@@ -7,12 +9,25 @@ dependencies {
     implementation(project(Modules.CORE_NAVIGATION))
     implementation(project(Modules.CORE_COMPONENTS))
     implementation(project(Modules.CORE_THEMES))
+    implementation(project(Modules.CORE_ABSTRACTION))
+
+    implementation(project(Modules.LIBRARY_NETWORK))
 
     implementationAndroidCore()
 
     implementationComposeUi()
 
+    implementationKotlinCoroutine()
+
+    implementation(Libs.DaggerHilt.DAGGER_HILT_ANDROID)
+    kapt(Libs.DaggerHilt.DAGGER_HILT_COMPILER)
+
     testImplementation()
 
     androidTestImplementation()
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
